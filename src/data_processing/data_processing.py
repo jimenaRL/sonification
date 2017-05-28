2353607f2add4425ecb0f24c695dd1859da738f6
jimena <jimena@melix.net>
2017-05-28 10:05:34 +0200

    new sr organsation

---
+++ src/data_processing/data_processing.py
@@ -0,0 +1,163 @@
+import os
+import sys
+import copy
+import datetime
+import glob
+import pandas as pd
+
+from data_utils import *
+
+
+def sex(df, columns):
+    """ Sex difference data traitement. """
+
+    # define sort criteria
+    sort_criteria = copy.deepcopy(columns)
+    sort_criteria.remove('Sex')
+    sort_criteria.remove('Value')
+
+    # get avalaible sexs
+    avalaible_sexs = list(set(df['Sex'].tolist()))
+    if not len(avalaible_sexs) > 1:
+        print "fail treatment by sex: %s" % df.Indicator.tolist()[0]
+        raise ValueError("Need at least two keys in 'Sex' columns.")
+    df = df[df['Sex'] != 'All']
+    if 'All' in avalaible_sexs:
+        avalaible_sexs.remove('All')
+    if not len(avalaible_sexs) == 2:
+        print "fail treatment by sex: %s" % df.Indicator.tolist()[0]
+        raise ValueError("Wrong number of keys in 'Sex' columns.")
+    sexA, sexB = avalaible_sexs
+
+    # pre difference treatment
+    df = add_hash(df, sort_criteria)
+
+    sexA_df = df[df['Sex'] == sexA].sort_values('hash')
+    sexA_df.rename(columns={u'Value': 'Value_A'}, inplace=True)
+    sexB_df = df[df['Sex'] == sexB].sort_values('hash')
+    sexB_df.rename(columns={u'Value': 'Value_B'}, inplace=True)
+
+    merged_sex = pd.merge(sexA_df, sexB_df, on=sort_criteria+['hash'])
+
+    # compute difference
+    merged_sex['diff'] = (merged_sex['Value_A'] - merged_sex['Value_B']).apply(lambda x: abs(x))
+
+    # scale difference to [0, 1] range
+    merged_sex['diff'] = (merged_sex['diff'] - merged_sex['diff'].min()) / \
+        (merged_sex['diff'].max() - merged_sex['diff'].min())
+
+    # store in new dataframe
+    merged_sex.rename(columns={u'diff': 'Value'}, inplace=True)
+    sex_diff_df = merged_sex[sort_criteria + ['Value']].copy(deep=True)
+
+    return sex_diff_df
+
+
+def share(df, columns):
+    pass
+
+
+def no_treat(df, columns):
+    return df
+
+
+def data_traitement(path, columns, treatment):
+
+    print 'processing \n\t%s' % path
+    print 'treatment \n\t%s' % treatment
+
+    if isinstance(columns, basestring):
+        columns = list(columns.split(','))
+
+    if isinstance(treatment, basestring):
+        treatment = globals()[treatment]
+
+    # load and clean data
+    df = pd.read_csv(path, delimiter=',', engine='python')[columns]
+    df.dropna(axis='index', inplace=True, how='any')
+
+    # normalize columns
+    df = normalize_columns(df)
+
+    # specific data traitement
+    processed_df = treatment(df, columns)
+    export_ML4(processed_df, path)
+
+
+def create_database():
+
+    if not os.path.exists(DATA_FOLDER):
+        raise ValueError("Please provide set a valid path for the data to treat.")
+
+    # create data dict
+    data = {
+        'education': {
+            'columns': COLUMNS + ['Sex'],
+            'paths':  glob.glob(os.path.join(DATA_FOLDER, "education/*.csv")),
+
+        },
+        'development': {
+            'columns': ['Country', 'Variables', 'Time', 'Value'],
+            'paths':  glob.glob(os.path.join(DATA_FOLDER, "development/*.csv")),
+
+        },
+        'employment': {
+            'columns': COLUMNS + ['Sex'],
+            'paths':  glob.glob(os.path.join(DATA_FOLDER, "employment/*.csv")),
+
+        },
+        'health': {
+            'columns': COLUMNS,
+            'paths':  glob.glob(os.path.join(DATA_FOLDER, "health/*.csv")),
+        },
+        'entrepreneurship': {
+            'columns': COLUMNS,
+            'paths':  glob.glob(os.path.join(DATA_FOLDER, "entrepreneurship/*.csv")),
+
+        },
+    }
+
+    for k, v in data.iteritems():
+        print k
+        verify_columns(v['paths'], v['columns'])
+
+    records = []
+    for kind, v in data.iteritems():
+        for path in v['paths']:
+            df = pd.read_csv(path, delimiter=',', engine='python')
+            df = normalize_columns(df)
+            records.append({
+                'path': path,
+                'columns': ','.join(v['columns']),
+                'kind': kind,
+                'name': df.Indicator.tolist()[0],
+                'code': os.path.split(path)[-1].split('.')[0],
+            })
+
+    df_out = pd.DataFrame.from_records(records)
+    columns = ['name', 'kind', 'code', u'columns', u'path']
+    path = '/Users/JRLetelier/perso/sonification/data/oced_all_' + \
+        datetime.datetime.now().strftime("%y%m%d-%Hh%M")+'.tsv'
+    df_out.to_csv(path, sep='\t', header=True, index=False, columns=columns)
+
+    if sys.platform == 'darwin':
+        os.system('open %s' % path)
+
+    print 'db saved at %s' % path
+
+
+def parse_database():
+
+    path = '/Users/JRLetelier/perso/sonification/data/oced/oced_all.csv'
+    db = pd.read_csv(path, delimiter=';', engine='python')
+
+    for k, row in db.iterrows():
+        if row['kind'] == 'development':
+            # try:
+            data_traitement(row.path, row['columns'], TREATMENTS[row.code]['treatment'])
+            # except Exception as e:
+            #     print e
+
+if __name__ == '__main__':
+    create_database()
+    # parse_database()
---
+++ src/data_processing/data_utils.py
@@ -0,0 +1,54 @@
+import os
+import pandas as pd
+
+DATA_FOLDER = '/Users/JRLetelier/perso/sonification/data/oced'
+
+COLUMNS = ['Country', 'Indicator', 'Time', 'Value']
+COLUMNS_RENAME_MAPPING = {'Variables': 'Indicator'}
+
+TREATMENTS_PATH = os.path.join(DATA_FOLDER, 'oced_code_treatment.csv')
+TREATMENTS = pd.read_csv(TREATMENTS_PATH, delimiter=';').set_index(['code']).to_dict('index')
+
+
+def normalize_columns(df):
+    return df.rename(columns=COLUMNS_RENAME_MAPPING, inplace=False)
+
+
+def verify_columns(paths, columns):
+    if paths:
+        columns_0 = pd.read_csv(paths[0], delimiter=',', engine='python').columns
+        for path in paths:
+            this_columns = pd.read_csv(path, delimiter=',', engine='python').columns
+            assert (columns_0 == this_columns).all()
+            assert not set(columns) - set(this_columns.tolist())
+    return True
+
+
+def export_ML4(df, path):
+    """ Export data to ML4 table format. """
+
+    # get colums for maxforlive format
+    df = df[COLUMNS]
+
+    # randomize rows
+    df = df.sample(frac=1)
+
+    # for OSC format
+    for column in df.columns:
+        df[column] = df[column].apply(lambda x: str(x).replace(' ', '_'))
+
+    pre_path, name = os.path.split(path.split('.')[0])
+    path_out = os.path.join(pre_path, name+'_m4l.tsv')
+
+    df.to_csv(path_out, sep='\t', header=False, index=False, encoding='utf-8', decimal=',')
+
+    print 'data saved at \n\t %s' % path_out
+
+
+def add_hash(df, hash_criteria):
+
+    df['hash'] = ''
+    for c in hash_criteria:
+        df['hash'] = df['hash'] + df[c].apply(lambda x: str(x))
+    df['hash'] = df['hash'].apply(lambda x: hash(x))
+    return df
