22c750afcc56e2907277ec87925465cee422b3a7
jimena <jimena@melix.net>
2017-06-28 23:33:20 +0200

    Add clear data

--- src/viz_server/server_fast.py
+++ src/viz_server/server_fast.py
@@ -1,5 +1,6 @@
 import sys
 import math
+import time
 import string
 import struct
 import socket
@@ -113,8 +114,15 @@ if __name__ == "__main__":
     while True:
         decoded = decodeOSC(sock.recv(1024))
         endpoint, types_ = decoded[:2]
-        country, measure, year, value = decoded[2:]
-        country = country.replace('_', ' ').replace('__', ' ')
-        measure = measure.replace('_', ' ').replace('__', ' ')
-        indent = np.random.randint(0, 10)
-        print ("\t" * indent + "%s %s %i %1.2f" % (country, measure, year, value) + '\n')
+        if endpoint == "/woman":
+            country, measure, year, value = decoded[2:]
+            country = country.replace('_', ' ').replace('__', ' ')
+            measure = measure.replace('_', ' ').replace('__', ' ')
+            indent = np.random.randint(0, 10)
+            print ("\t" * indent + "%s %s %i %1.2f" % (country, measure, year, value) + '\n')
+        elif endpoint == "/clean":
+            # speed in microseconds
+            for i in range(200):
+                print '\n'
+        else:
+            raise ValueError("Wrong endpoint %s. Must be '/woman' or 'clean'.")
